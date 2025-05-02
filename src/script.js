6b5479aea3f51358aade30a18639aed5adc8553f
BebsYanski <bebongnchuy@gmail.com>
2025-05-02 05:24:54 +0100

    feat: done with the darkmode display

--- src/script.js
+++ src/script.js
@@ -1,5 +1,35 @@
 const settingsMenu = document.querySelector(".settings-menu");
+const darkBtn = document.getElementById("dark-btn");
+let darkMode = false;
 
 function settingsMenuToggle() {
   settingsMenu.classList.toggle("settings-menu-height");
+}
+
+darkBtn.onclick = function () {
+  darkBtn.classList.toggle("dark-btn-on");
+  document.body.classList.toggle("dark-theme");
+
+  if (localStorage.getItem("darkMode") == "true" || darkMode == true || darkMode == "true") {
+    localStorage.setItem("darkMode", "false");
+  } else {
+    localStorage.setItem("darkMode", "true");
+  }
+
+  darkMode = !darkMode;
+};
+
+// function darkTheme() {
+//   darkBtn.classList.add("dark-btn-on");
+//   document.body.classList.add("dark-theme");
+// }
+
+if (localStorage.getItem("darkMode") == "true") {
+  darkBtn.classList.add("dark-btn-on");
+  document.body.classList.add("dark-theme");
+} else if (localStorage.getItem("darkMode") == "false") {
+  darkBtn.classList.remove("dark-btn-on");
+  document.body.classList.remove("dark-theme");
+} else {
+  localStorage.setItem("darkMode", darkMode.toString());
 }
